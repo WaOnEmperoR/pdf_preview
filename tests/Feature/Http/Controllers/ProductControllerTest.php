023f9aee842cdc52d345607d40e8304f8597a8c8
Rachmawan Atmaji <themusketeer17@gmail.com>
2020-04-09 14:33:00 +0700

    test qty error

--- tests/Feature/Http/Controllers/ProductControllerTest.php
+++ tests/Feature/Http/Controllers/ProductControllerTest.php
@@ -54,4 +54,32 @@ class ProductControllerTest extends TestCase
         //Tuntutan bahwa abis melakukan POST URL akan dialihkan ke URL product atau routenya adalah product.index
         $response->assertRedirect(route('product.index'));
     }
+
+    /**
+     * @test
+     */
+    public function test_store_qty_err()
+    {
+        //TODO: code inside here --Created by Kiddy
+    
+        //Membuat objek user yang otomatis menambahkannya ke database.
+        $user = factory(User::class)->create();
+
+        //Membuat objek category yang otomatis menambahkannya ke database.
+        $category = factory(Category::class)->create();
+
+        //Acting as berfungsi sebagai autentikasi, jika kita menghilangkannya maka akan error.
+        $response = $this->actingAs($user)
+        //Hit post ke method store, fungsinya ya akan lari ke fungsi store.
+        ->post(route('product.store'), [
+            //isi parameter sesuai kebutuhan request
+            'name' => $this->faker->words(3, true),
+            'cat' => $category->id,
+            'quantity' => $this->faker->words(3),
+            'buy_price' => $this->faker->randomNumber(6),
+            'sell_price' => $this->faker->randomNumber(6),
+        ]);
+
+        $response->assertStatus(500);
+    }
 }
